75a418d8368add82d3c41412eeeb3222f5878f7c
Mathilde LORRAIN <mlorrain@takima.fr>
2020-04-20 13:13:59 +0200

    fix : script data words

--- resources/db/sql/V1_5__word_paird_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO word_pairs values (1, 'chien','chat')
-INSERT INTO word_pairs values (2, 'tee-shirt','pantalon')
---
+++ resources/db/sql/V1_5__word_pairs_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO word_pairs values (1, 'chien','chat');
+INSERT INTO word_pairs values (2, 'tee-shirt','pantalon');
